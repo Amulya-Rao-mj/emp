bfa4ad0a74d756fc1469ddbc5c5aeeff98c0b5b9
Amulya Rao M J <2023ci_amulyaraomj_a@nie.ac.in>
2024-09-12 12:45:15 +0530

    added update

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Employee
 from .forms import EmployeeForm
 
@@ -17,3 +17,22 @@ def add_employee(request):
         form = EmployeeForm()
 
     return render(request, 'add_emp.html', {'form':form})    
+
+def delete_emp(request, pk):
+    employee = get_object_or_404(Employee, pk=pk)
+    if request.method == 'POST':
+        employee.delete()
+        return redirect('emp_list')
+    return render(request, 'delete_emp.html',{'employee':employee})
+
+def update_emp(request, pk):
+    employee = get_object_or_404(Employee, pk=pk)
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST, instance=employee)
+        if form.is_valid():
+            form.save()
+            return redirect('emp_list')
+    else:
+        form = EmployeeForm(instance=employee)
+
+    return render(request, 'update_emp.html', {'form':form, 'employee':employee})    
